409a8c0f1a492c7c4a364dc3057eea99bb9dfd9f
Adrian Eguez <eadepto@hotmail.com>
2021-02-17 18:29:28 -0500

    clases

--- clases/src/app.controller.ts
+++ clases/src/app.controller.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
 }
+
+// Clases - TYPESCRIPT
+
+abstract class Nombre {
+    public nombrePropiedad?: string; // undefined
+    private apellidoPropiedad: string = 'Eguez';
+    protected edad = 1; // number (Duck Typing)
+    static comun: number = 10;
+
+    propiedadPublica: string;
+
+    constructor(
+        propiedadPublicaParametro: string, // parametro
+        public propiedadRapido: string, // transforma una propiedad
+    ) {
+        this.propiedadPublica = propiedadPublicaParametro;
+        // this.propiedadPublicaParametro ERROR
+        // this.propiedadRapido; OK
+    }
+
+    public funcionPublica(parametroString: string): void {
+        // no hay return = undefined
+    }
+
+    private funcionPrivada(parametroString: string, // ? = puede ser undefined
+                           parametroNumber?: number)  { // omitir :void (defecto)
+        // no hay return = undefined
+    }
+
+    protected funcionPublica(): number {
+        return 1;
+    }
+
+    static funcionEstatica(): string {
+        return 'string';
+    }
+
+}
+
+
+/**/
